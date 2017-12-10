84159a57ad003e59ca023e731383923ae3836aca
YWHo <YWHo@users.noreply.github.com>
2017-12-11 00:58:20 +1300

    update comment

--- Advance/Validate_US_Telephone_Numbers.js
+++ Advance/Validate_US_Telephone_Numbers.js
@@ -1,6 +1,10 @@
 /* Return true if the passed string is a valid US phone number.
 
-For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.
+For this challenge you will be presented with a string such as 800-692-7753 or 
+8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based
+on any combination of the formats provided above. The area code is required. If the
+country code is provided, you must confirm that the country code is 1. Return true
+if the string is a valid US phone number; otherwise return false.
 
 The following are examples of valid formats for US numbers:
  555-555-5555
