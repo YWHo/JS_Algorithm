97c43076a30b6359f697ff0f0f7ebca5c18fc802
YWHo <YWHo@users.noreply.github.com>
2017-12-10 23:46:56 +1300

    Add 'Sum_All_Numbers_in_a_Range'

---
+++ Intermediate/Sum_All_Numbers_in_a_Range.js
@@ -0,0 +1,17 @@
+//an array of two numbers. Return the sum of those two numbers and all numbers between them.
+// The lowest number will not always come first.
+
+function sumAll(arr) {
+
+    var min = Math.min(arr[0], arr[1]);
+    var max = Math.max(arr[0], arr[1]);
+    var result = 0;
+
+    for (var i=min; i<=max; i++) {
+        result += i;
+    }
+    return result;
+}
+
+var x = sumAll([1, 4]);
+console.log(x);
